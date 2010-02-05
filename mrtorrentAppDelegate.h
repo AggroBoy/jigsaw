0916f1b2d6e130073d22bf564eeddcf6b380960e
will <will@bf3e2900-abe3-4bc4-868a-2698488a2087>
2010-02-05 20:23:00 +0000

    Allow adding torrent files, both from the menu, and by file association.
    git-svn-id: file:///var/svn-repos/cocoa/mrtorrent/trunk/mrtorrent@17 bf3e2900-abe3-4bc4-868a-2698488a2087

--- mrtorrentAppDelegate.h
+++ mrtorrentAppDelegate.h
@@ -6,12 +6,25 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "TorrentListModel.h"
 
 @interface mrtorrentAppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
+	IBOutlet TorrentListModel *torrentList;
+	
+	NSString *fileToOpen;
 }
 
 - (BOOL)applicationShouldHandleReopen:(NSApplication *)theApplication hasVisibleWindows:(BOOL)flag;
+
+- (BOOL)application:(NSApplication *)theApplication openFile:(NSString *)filename;
+
+- (void)addLocalTorrentFile:(NSString *)fileName;
+
+- (IBAction)addTorrentFile:(id)sender;
+
+- (IBAction)addTorrentURL:(id)sender;
+
 
 @property (assign) IBOutlet NSWindow *window;
 
--- mrtorrentAppDelegate.m
+++ mrtorrentAppDelegate.m
@@ -4,6 +4,7 @@
 //
 //  Created by Will Goring on 06/12/2009.
 //
+#import "XMLRPC/XMLRPC.h"
 
 #import "mrtorrentAppDelegate.h"
 
@@ -12,7 +13,7 @@
 @synthesize window;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	// Insert code here to initialize your application 
+	// Init code goes here
 }
 
 - (BOOL)applicationShouldHandleReopen:(NSApplication *)theApplication hasVisibleWindows:(BOOL)flag
@@ -23,6 +24,59 @@
 		[window makeKeyAndOrderFront:self];
 	}
 	return NO;
+}
+
+- (BOOL)application:(NSApplication *)theApplication openFile:(NSString *)filename
+{
+	[self addLocalTorrentFile:filename];
+	return YES;
+}
+
+- (void)addLocalTorrentFile:(NSString *)filename
+{
+	NSLog(@"%@", filename);
+	NSData *torrentData = [[NSFileManager defaultManager] contentsAtPath:filename];
+	
+	NSURL *URL = [NSURL URLWithString:@"http://horus/RPC2"];
+	XMLRPCRequest *request = [[XMLRPCRequest alloc] initWithURL: URL];
+	
+	[request setMethod: @"load_raw_start" withParameter:torrentData];
+	[XMLRPCConnection sendSynchronousXMLRPCRequest:request];
+	
+	[torrentList update];
+}
+
+- (IBAction)addTorrentFile:(id)sender
+{
+	// Create the File Open Dialog class.
+	NSOpenPanel* openDlg = [NSOpenPanel openPanel];
+	
+	// Enable the selection of files in the dialog.
+	[openDlg setCanChooseFiles:YES];
+	
+	// Enable the selection of directories in the dialog.
+	[openDlg setCanChooseDirectories:NO];
+	
+	// Disable Multiple Selection
+	[openDlg setAllowsMultipleSelection:NO];
+	
+	// Types we allow
+	NSArray* fileType = [NSArray arrayWithObjects: @"torrent", nil];
+	
+	// Display the dialog. If the OK button was pressed,
+	// process the files.
+	if ( [openDlg runModalForDirectory:nil file:nil types:fileType] == NSOKButton )
+	{
+		NSArray *files = [openDlg URLs];
+		NSURL *url = [files objectAtIndex:0];
+		NSString *fileName = [url path];
+		[self addLocalTorrentFile:fileName];
+	}
+}
+
+- (IBAction)addTorrentURL:(id)sender
+{
+	
 }
 
 @end
